7723a6bfe2e56ee97e93be7cb20ce1bcefdb0180
Flo-Bac <florian-bachmann@bluewin.ch>
2023-08-01 06:14:47 -0400

    Arbeit an Final assignment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
+
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    //const { budget } = useContext(AppContext);
+    const [budget, setBudget]  = useState(2000);
+
+    if(budget > 20000) {
+        alert("The value cannot exceed 20'000");
+        setBudget(20000)
+        return;
+    }
+
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+        <div className={`alert alert-secondary`}>
+        <span>Budget:
+        
+        <input
+        required='required'
+        type='number'
+        id='budget'
+        value={budget}
+        style={{ marginLeft: '2rem' , size: 10}}
+        onChange={(event) => setBudget(event.target.value)}>
+        </input>
+        </span>
         </div>
     );
 };
+
 export default Budget;
