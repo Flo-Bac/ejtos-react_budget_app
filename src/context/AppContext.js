21f85cf9dd3cd7e67880b3058d05da222ace521b
Flo-Bac <140746307+Flo-Bac@users.noreply.github.com>
2023-08-09 09:48:24 +0200

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -70,9 +70,9 @@ export const AppReducer = (state, action) => {
 
         case 'CHG_CURRENCY':
             action.type = "DONE";
+            state.currency = action.payload
             return {
                 ...state,
-                currency: action.payload,
             };
 
         default:
