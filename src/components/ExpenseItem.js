d6b337a3c3ccc9a846bfaef2ae328c59925cdef7
Flo-Bac <140746307+Flo-Bac@users.noreply.github.com>
2023-08-09 12:45:20 +0200

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FaMinusCircle, FaPlusCircle} from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
     const { dispatch, currency } = useContext(AppContext);
@@ -18,8 +19,21 @@ const ExpenseItem = (props) => {
             cost: 10,
         };
 
+
         dispatch({
             type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
+    
+     const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+            };
+
+        dispatch({
+            type: "RED_EXPENSE",
             payload: expense
         });
 
@@ -29,10 +43,12 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><FaPlusCircle size="2.5em" color="green" onClick={event=> increaseAllocation(props.name)}>+</FaPlusCircle></td>
+        <td><FaMinusCircle size="2.5em" color="red" onClick={event=> decreaseAllocation(props.name)}>-</FaMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
-};
+ };
 
 export default ExpenseItem;
+
